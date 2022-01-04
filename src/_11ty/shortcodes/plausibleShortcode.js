f190b62aa35cbd4b9100150875dd33159089c0fd
Brian Wisti <brianwisti@pobox.com>
2022-01-03 23:04:02 -0800

    footer layout; social metadata

--- src/_11ty/shortcodes/plausibleShortcode.js
+++ src/_11ty/shortcodes/plausibleShortcode.js
@@ -2,7 +2,7 @@ module.exports = () => {
   let analyticsWanted = process.env.INCLUDE_ANALYTICS;
 
   if (analyticsWanted) {
-    return `<script async defer data-domain="https://randomgeekery.org"
+    return `<script async defer data-domain="randomgeekery.org"
       src="https://plausible.io/js/plausible.js">
     </script>`;
   }
